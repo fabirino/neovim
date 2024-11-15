d5cfa8cb70c3cd3e7b0833f9707067345704044f
FabioS <fabalexandre8@gmail.com>
2024-11-15 15:09:58 +0000

    Lualine update

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,6 +1,17 @@
 require("lualine").setup {
     options = {
         theme = "auto",
-        --theme = "palenight"
-    }
+        -- theme = "palenight"
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' }
+    },
+    -- sections = {
+    --     lualine_a = {'mode'},
+    --     lualine_b = {}, -- Remova o Git aqui
+    --     lualine_c = {'filename'},
+    --     lualine_x = {'encoding', 'fileformat', 'filetype'},
+    --     lualine_y = {'progress'},
+    --     lualine_z = {'location'}
+    -- }
 }
+
